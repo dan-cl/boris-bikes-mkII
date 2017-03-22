6f1a40b5c25ec250603e004572359667abf85350
Daniel Clissold <dan@Daniels-MacBook-Pro.local>
2017-03-22 15:39:56 +0000

    14. Complex Attributes

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -14,18 +14,14 @@ describe DockingStation do
     expect(subject).to respond_to(:dock).with(1).argument
   end
 
-  it { is_expected.to respond_to(:bike) }
+
 
   it 'docks something' do
     bike = Bike.new
-    expect(subject.dock(bike)).to eq bike
+    expect(subject.dock(bike)).to include bike
   end
 
-  it 'returns docked bikes' do
-    bike = Bike.new
-    subject.dock(bike)
-    expect(subject.bike).to eq bike
-  end
+  
 
   describe '#release_bike' do
     it 'releases a bike' do
@@ -41,7 +37,7 @@ describe DockingStation do
 
   describe '#dock' do
     it 'raises an error when full' do
-      subject.dock(Bike.new)
+      20.times {subject.dock(Bike.new)}
       expect {subject.dock Bike.new}.to raise_error 'Docking station full'
     end
   end
