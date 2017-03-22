9db48cf24826161dd92a36ec7d60f16af3722317
Daniel Clissold <dan@Daniels-MacBook-Pro.local>
2017-03-22 15:02:31 +0000

    12. Raising Exceptions

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -6,7 +6,7 @@ describe DockingStation do
   end
 
   it 'releases working bikes' do
-    bike = subject.release_bike
+    bike = Bike.new
     expect(bike).to be_working
   end
 
@@ -25,6 +25,18 @@ describe DockingStation do
     bike = Bike.new
     subject.dock(bike)
     expect(subject.bike).to eq bike
+  end
+
+  describe '#release_bike' do
+    it 'releases a bike' do
+      bike=Bike.new
+      subject.dock(bike)
+      expect(subject.release_bike).to eq bike
+    end
+
+    it 'raises an error when there are no bikes available' do
+      expect {subject.release_bike}.to raise_error 'No bikes available'
+    end
   end
 
 end
