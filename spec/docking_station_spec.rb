6b9668576eac76ecdd48eed0a0e6b784e85ff2cc
Daniel Clissold <dan@Daniels-MacBook-Pro.local>
2017-03-22 18:28:10 +0000

    17. Initializtion defaults

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -21,6 +21,10 @@ describe DockingStation do
     expect(subject.dock(bike)).to include bike
   end
 
+  it 'has a default capacity' do
+    expect(subject.capacity).to eq DockingStation::DEFAULT_CAPACITY
+  end
+
 
 
   describe '#release_bike' do
@@ -37,10 +41,24 @@ describe DockingStation do
 
   describe '#dock' do
     it 'raises an error when full' do
-      DockingStation::DEFAULT_CAPACITY.times do
-        subject.dock Bike.new
-      end
+      subject.capacity.times { subject.dock Bike.new }
       expect {subject.dock Bike.new}.to raise_error 'Docking station full'
+    end
+  end
+
+  describe 'initialization' do
+    subject { DockingStation.new}
+    let(:bike) { Bike.new }
+    it 'defaults capacity' do
+      described_class::DEFAULT_CAPACITY.times do
+        subject.dock(bike)
+      end
+      expect{ subject.dock(bike) }.to raise_error 'Docking station full'
+    end
+    it 'has a variable capacity' do
+      docking_station = DockingStation.new(50)
+      50.times { docking_station.dock Bike.new }
+      expect{ docking_station.dock Bike.new }.to raise_error 'Docking station full'
     end
   end
 
