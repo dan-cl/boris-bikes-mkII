31c21f3890e32ff208e04954494f592efcd62cf6
Daniel Clissold <dan@Daniels-MacBook-Pro.local>
2017-03-23 13:50:49 +0000

    18. Dealing with Broken Bikes

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -12,6 +12,7 @@ class DockingStation
 
   def release_bike
     fail 'No bikes available' if empty?
+    fail 'No bikes available' unless working?
     bikes.pop
   end
 
@@ -30,6 +31,10 @@ class DockingStation
 
   def empty?
     bikes.empty?
+  end
+
+  def working?
+    bikes.last.working?
   end
 
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -37,6 +37,13 @@ describe DockingStation do
     it 'raises an error when there are no bikes available' do
       expect {subject.release_bike}.to raise_error 'No bikes available'
     end
+
+    it 'will not release a broken bike' do
+      bike=Bike.new
+      bike.report_broken
+      subject.dock(bike)
+      expect {subject.release_bike}.to raise_error 'No bikes available'
+    end
   end
 
   describe '#dock' do
